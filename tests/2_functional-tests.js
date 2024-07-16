f6a16ceab055fffa0f04a29f40fd2f01b4b54ba3
Puchana <puchana123@gmail.com>
2024-07-16 17:51:31 +0700

    feat: add PUT request test

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -8,7 +8,7 @@ chai.use(chaiHttp);
 
 suite('Functional Tests', function () {
     this.timeout(5000);
-    suite('POST: /api/issues/apitest', () => {
+    suite('POST', () => {
         // #1
         test('Create an issue with every field', () => {
             chai
@@ -22,10 +22,10 @@ suite('Functional Tests', function () {
                     assigned_to: 'ApiTester',
                     status_text: 'test'
                 })
-                .end((err,res)=>{
-                    const output = res.body
+                .end((err, res) => {
+                    const output = res.body;
                     assert.equal(res.status, 200, 'api should response');
-                    assert.isObject(output,'output should be Object');
+                    assert.isObject(output, 'output should be Object');
                     assert.equal(output.issue_title, 'Api_test1');
                     assert.equal(output.issue_text, 'this is an api test #1');
                     assert.equal(output.created_by, 'ApiTester');
@@ -47,14 +47,14 @@ suite('Functional Tests', function () {
                     issue_text: 'this is an api test #2',
                     created_by: 'ApiTester',
                 })
-                .end((err,res)=>{
+                .end((err, res) => {
                     const output = res.body
                     assert.equal(res.status, 200, 'api should response');
-                    assert.isObject(output,'output should be Object');
+                    assert.isObject(output, 'output should be Object');
                     assert.equal(output.issue_title, 'Api_test2');
                     assert.equal(output.issue_text, 'this is an api test #2');
                     assert.equal(output.created_by, 'ApiTester');
-                    assert.property(output,'assigned_to');
+                    assert.property(output, 'assigned_to');
                     assert.property(output, 'status_text');
                     assert.property(output, 'created_on');
                     assert.property(output, 'updated_on');
@@ -71,23 +71,23 @@ suite('Functional Tests', function () {
                     assigned_to: 'ApiTester',
                     status_text: 'test'
                 })
-                .end((err,res)=>{
+                .end((err, res) => {
                     const output = res.body
                     assert.equal(res.status, 200, 'api should response');
-                    assert.isObject(output,'output should be Object');
-                    assert.property(output,'error');
+                    assert.isObject(output, 'output should be Object');
+                    assert.property(output, 'error');
                     assert.equal(output.error, 'required field(s) missing');
                 })
         });
     });
-    suite('GET: /api/issues/apitest', () => {
+    suite('GET', () => {
         // #4
         test('View all issues on a project', () => {
             chai
                 .request(server)
                 .keepOpen()
                 .get(api_url)
-                .end((err,res)=>{
+                .end((err, res) => {
                     const output = res.body
                     assert.equal(res.status, 200, 'api should response');
                     assert.equal(res.type, "application/json");
@@ -97,58 +97,132 @@ suite('Functional Tests', function () {
         // #5
         test('View issuses on a project with one filter', () => {
             chai
-            .request(server)
-            .keepOpen()
-            .get(api_url)
-            .query({'issue_title':'Api_test1'})
-            .end((err,res)=>{
-                const output = res.body
-                assert.equal(res.status, 200, 'api should response');
-                assert.equal(res.type, "application/json");
-                assert.isArray(output, 'output is array of object');
-                assert.equal(output[0].issue_title, 'Api_test1' ,'filter with issue_title');
-            })
+                .request(server)
+                .keepOpen()
+                .get(api_url)
+                .query({ 'issue_title': 'Api_test1' })
+                .end((err, res) => {
+                    const output = res.body
+                    assert.equal(res.status, 200, 'api should response');
+                    assert.equal(res.type, "application/json");
+                    assert.isArray(output, 'output is array of object');
+                    assert.equal(output[0].issue_title, 'Api_test1', 'filter with issue_title');
+                })
         });
         // #6
         test('View issuses on a project with mulitiple filters', () => {
             chai
-            .request(server)
-            .keepOpen()
-            .get(api_url)
-            .query({
-                'issue_title':'Api_test1',
-                'status_text': 'test'
-            })
-            .end((err,res)=>{
-                const output = res.body
-                assert.equal(res.status, 200, 'api should response');
-                assert.equal(res.type, "application/json");
-                assert.isArray(output, 'output is array of object');
-                assert.equal(output[0].issue_title, 'Api_test1' ,'filter with issue_title');
-                assert.equal(output[0].status_text, 'test' ,'filter with status_text');
-            })
+                .request(server)
+                .keepOpen()
+                .get(api_url)
+                .query({
+                    'issue_title': 'Api_test1',
+                    'status_text': 'test'
+                })
+                .end((err, res) => {
+                    const output = res.body
+                    assert.equal(res.status, 200, 'api should response');
+                    assert.equal(res.type, "application/json");
+                    assert.isArray(output, 'output is array of object');
+                    assert.equal(output[0].issue_title, 'Api_test1', 'filter with issue_title');
+                    assert.equal(output[0].status_text, 'test', 'filter with status_text');
+                })
         });
     });
-    suite('PUT: /api/issues/apitest', () => {
+    suite('PUT', () => {
+        const id = '66963c03c8c35ad6e6720c65';
         // #7
         test('Update one field on an issue', () => {
-            assert.fail();
+            chai
+                .request(server)
+                .keepOpen()
+                .put(api_url)
+                .send({
+                    _id: id,
+                    issue_title: 'Api_Test3'
+                })
+                .end((err, res) => {
+                    const output = res.body
+                    assert.equal(res.status, 200, 'api should response');
+                    assert.equal(res.type, "application/json");
+                    assert.isObject(output, 'output is an object');
+                    assert.propertyVal(output, 'result', 'successfully updated');
+                    assert.propertyVal(output, '_id', id);
+                })
         });
         // #8
         test('Update multiple fields on an issue', () => {
-            assert.fail();
+            chai
+                .request(server)
+                .keepOpen()
+                .put(api_url)
+                .send({
+                    _id: id,
+                    issue_title: 'Api_Test4',
+                    issue_text: 'This is update 3 times'
+                })
+                .end((err, res) => {
+                    const output = res.body
+                    assert.equal(res.status, 200, 'api should response');
+                    assert.equal(res.type, "application/json");
+                    assert.isObject(output, 'output is an object');
+                    assert.propertyVal(output, 'result', 'successfully updated');
+                    assert.propertyVal(output, '_id', id);
+                })
         });
         // #9
         test('Update an issue with missing _id', () => {
-            assert.fail();
+            chai
+                .request(server)
+                .keepOpen()
+                .put(api_url)
+                .send({
+                    issue_title: 'Api_Test5'
+                })
+                .end((err, res) => {
+                    const output = res.body
+                    assert.equal(res.status, 200, 'api should response');
+                    assert.equal(res.type, "application/json");
+                    assert.isObject(output, 'output is an object');
+                    assert.propertyVal(output, 'error', 'missing _id');
+                })
         });
         // #10
         test('Update an issue with no fields to update', () => {
-            assert.fail();
+            chai
+                .request(server)
+                .keepOpen()
+                .put(api_url)
+                .send({
+                    _id: id
+                })
+                .end((err, res) => {
+                    const output = res.body
+                    assert.equal(res.status, 200, 'api should response');
+                    assert.equal(res.type, "application/json");
+                    assert.isObject(output, 'output is an object');
+                    assert.propertyVal(output, 'error', 'no update field(s) sent');
+                    assert.propertyVal(output, '_id', id);
+                })
         });
         // #11
         test('Update an issue with an invalid _id', () => {
-            assert.fail();
+            chai
+                .request(server)
+                .keepOpen()
+                .put(api_url)
+                .send({
+                    _id: '123456',
+                    issue_title: 'Api_Test6'
+                })
+                .end((err, res) => {
+                    const output = res.body
+                    assert.equal(res.status, 200, 'api should response');
+                    assert.equal(res.type, "application/json");
+                    assert.isObject(output, 'output is an object');
+                    assert.propertyVal(output, 'error', 'could not update');
+                    assert.propertyVal(output, '_id', '123456');
+                })
         });
     });
     suite('DELETE: /api/issues/apitest', () => {
